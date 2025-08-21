4002bc38a7fe09e93622dc0c11fa61f8c6a5abae
steffen911 <steffen@langfuse.com>
2025-08-21 09:54:07 +0200

    chore: revert quickstart setup

--- examples/quickstart/quickstart.tf
+++ examples/quickstart/quickstart.tf
@@ -1,7 +1,7 @@
 module "langfuse" {
   source = "../.."
 
-  domain = "aws-experiment.langfuse.dev"
+  domain = "langfuse.example.com"
 
   # Optional use a different name for your installation
   # e.g. when using the module multiple times on the same AWS account
@@ -12,7 +12,7 @@ module "langfuse" {
 
   # Optional: Configure the VPC
   vpc_cidr               = "10.0.0.0/16"
-  use_single_nat_gateway = true # Using a single NAT gateway decreases costs, but is less resilient
+  use_single_nat_gateway = false # Using a single NAT gateway decreases costs, but is less resilient
 
   # Optional: Configure the Kubernetes cluster
   kubernetes_version         = "1.32"
@@ -22,8 +22,6 @@ module "langfuse" {
   postgres_instance_count = 2
   postgres_min_capacity   = 0.5
   postgres_max_capacity   = 2.0
-
-  clickhouse_replicas = 2
 
   # Optional: Configure the cache
   cache_node_type      = "cache.t4g.small"
