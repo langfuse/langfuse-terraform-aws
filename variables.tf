c3868ebee628ebdb3c426f9edb4c0491dfdec656
James Stonehill <jamesstonehill@gmail.com>
2026-05-08 21:13:29 +0100

    Update helm chart version

--- variables.tf
+++ variables.tf
@@ -144,7 +144,7 @@ variable "use_single_nat_gateway" {
 variable "langfuse_helm_chart_version" {
   description = "Version of the Langfuse Helm chart to deploy"
   type        = string
-  default     = "1.5.14"
+  default     = "1.5.29"
 }
 
 # Resource configuration variables
